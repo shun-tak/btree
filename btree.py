1ece9883299f1e4ce11f7d8c09cb9d10d3a6ee91
shun-tak <shun.tak221@gmail.com>
2018-04-11 20:14:53 +0900

    整理

--- btree.py
+++ btree.py
@@ -5,34 +5,30 @@ https://ysangkok.github.io/js-clrs-btree/btree.html
 class BTree:
   VISITED = set([])
 
-  def __init__(self, order=None, children=None, keys=None):
+  def __init__(self, order=None, root=None):
     self.order = order or 1
-    self.children = children or []
-    self.keys = keys or []
+    self.root = root or Node()
 
   def __repr__(self):
-    ret = ""
-    ret += ", ".join(map(str, self.keys))
+    ret = "- [id:" + str(self.root.id) + "] "
+    ret += ", ".join(map(str, self.root.keys))
     ret += "\n"
     BTree.VISITED = set([])
-    for child in self.children:
-      ret += self._dfs_repr(child)
+    for child in self.root.children:
+      ret += self._repr_dfs(child)
     return ret
 
-  def _dfs_repr(self, node, depth=0):
+  def _repr_dfs(self, node, depth=0):
     depth += 1
     ret = ""
-    ret += "  " * depth
-    ret += "- "
+    ret += "  " * depth + "- [id:" + str(node.id) + "] "
     ret += ", ".join(map(str, node.keys))
     ret += "\n"
     BTree.VISITED.add(node.id)
     for child in node.children:
       if not child.id in BTree.VISITED:
-        ret += self._dfs_repr(child, depth)
+        ret += self._repr_dfs(child, depth)
     return ret
-
-#   def validate(self):
 
 
 class Node:
@@ -46,7 +42,7 @@ class Node:
 
 
 tree = BTree(order=5, \
-  keys=[10], children=[\
+  root=Node(keys=[10], children=[\
     Node(keys=[3,6], children=[\
       Node(keys=[0,1,2]), \
       Node(keys=[4,5]), \
@@ -57,6 +53,6 @@ tree = BTree(order=5, \
       Node(keys=[15,16]), \
       Node(keys=[18,19,20]), \
       Node(keys=[22,23])\
-    ])])
+    ])]))
 
 print(tree)
